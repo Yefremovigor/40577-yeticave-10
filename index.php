a51eae99f9b91395b8abe50429bce62ffcf03492
Игорь <Yefremovigor@yandex.ru>
2019-08-10 20:22:07 +0300

    Добавляет вывод обявлений на главную

--- index.php
+++ index.php
@@ -95,7 +95,6 @@ $ads = [
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
             <?php foreach ($categories as $key => $val): ?>
                 <li class="promo__item promo__item--boards">
                     <a class="promo__link" href="pages/all-lots.html"><?=$val; ?></a>
@@ -108,25 +107,26 @@ $ads = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
+            <?php foreach ($ads as $key => $item): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$item['img']; ?>" width="350" height="260" alt="<?=$item['title']; ?>">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$item['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['title']; ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$item['price']; ?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -135,7 +135,6 @@ $ads = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
             <?php foreach ($categories as $key => $val): ?>
                 <li class="nav__item">
                     <a href="pages/all-lots.html"><?=$val; ?></a>
