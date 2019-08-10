981cd538e9169454c737ba30bf86f338a545c56e
Игорь <Yefremovigor@yandex.ru>
2019-08-10 19:26:22 +0300

    Увеличивает уровень вложенности у кода для отображения логина

--- index.php
+++ index.php
@@ -27,24 +27,22 @@ $user_name = 'Игорь';
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <?php if ($is_auth === 1): ?>
-            <div class="user-menu__logged">
-                <p><?=$user_name; ?></p>
-                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-                <a class="user-menu__logout" href="#">Выход</a>
-            </div>
-        <?php else: ?>
-            <ul class="user-menu__list">
-                <li class="user-menu__item">
-                    <a href="#">Регистрация</a>
-                </li>
-                <li class="user-menu__item">
-                    <a href="#">Вход</a>
-                </li>
-            </ul>
-        <?php endif; ?>
-
+            <?php if ($is_auth === 1): ?>
+                <div class="user-menu__logged">
+                    <p><?=$user_name; ?></p>
+                    <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                    <a class="user-menu__logout" href="#">Выход</a>
+                </div>
+            <?php else: ?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                        <a href="#">Регистрация</a>
+                    </li>
+                    <li class="user-menu__item">
+                        <a href="#">Вход</a>
+                    </li>
+                </ul>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
