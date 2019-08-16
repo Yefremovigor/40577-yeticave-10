861896a37e658c1b8157829a541e544a45f450f6
Игорь <Yefremovigor@yandex.ru>
2019-08-16 15:55:02 +0300

    Добавляет второй параметр ENT_QUOTES в фильтр htmlspecialchars

--- templates/main.php
+++ templates/main.php
@@ -18,15 +18,15 @@
             <?php $lot_timer = get_dt_range($item['auction_end_date']) ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$item['img'] ?>" width="350" height="260" alt="<?=htmlspecialchars($item['title']) ?>">
+                    <img src="<?=$item['img'] ?>" width="350" height="260" alt="<?=htmlspecialchars($item['title'], ENT_QUOTES) ?>">
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=$item['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($item['title']) ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($item['title'], ENT_QUOTES) ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=htmlspecialchars(set_price_format($item['price'])) ?></span>
+                            <span class="lot__cost"><?=set_price_format($item['price']) ?></span>
                         </div>
                         <div class="lot__timer timer <?=is_ad_finishing($lot_timer['hour']) ?>">
                             <?=$lot_timer['hour'].':'.$lot_timer['minute'] ?>
