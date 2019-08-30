d1ca2991f81046c7db47f0e6cbaa9cdb6ed50609
Игорь <Yefremovigor@yandex.ru>
2019-08-30 12:05:13 +0300

    Заменят накопление ошибок в списке на die()

--- db.php
+++ db.php
@@ -5,5 +5,5 @@ $db_connect = mysqli_connect('127.0.0.1:8889', 'root', 'root','yeticave');
 if ($db_connect) {
     mysqli_set_charset($db_connect, 'utf8');
 } else {
-    $errors[] = 'Ошибка подключения к БД: ' . mysqli_connect_error();
+    die('Ошибка подключения к БД: ' . mysqli_connect_error());
 }
