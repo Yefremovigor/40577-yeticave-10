a20ca42c1c7c71532f66e39ff3709f0d9d1c62b2
Игорь <Yefremovigor@yandex.ru>
2019-08-30 12:09:57 +0300

    Убирает вывод ошибок в шаблоне заменив обработку ошибок на die()

--- index.php
+++ index.php
@@ -1,62 +1,46 @@
 <?php
-// Создаем массив для хранения возможных ошибок.
-$errors = [];
-
-// Объявляем все переменные необходимые для отображения layout.
-$user_name = 'Игорь';
-$categories = [];
-$content = '';
-
 // Проверямем авторизацию.
 $is_auth = rand(0, 1);
+$user_name = 'Игорь';
 
 // Подключаем базу данных.
 include_once('db.php');
 // Поподключаем функции.
 include_once('helpers.php');
 include_once('functions.php');
-
 // Задаем часовой пояс
 date_default_timezone_set('Europe/Moscow');
 
-if ($db_connect) {
-    // Собираем запрос для получения саиска лотов для главной.
-    $ads_sql = 'SELECT lots.title, categories.name AS category, lots.start_price AS price,'
-        . ' lots.img, lots.finish_date AS auction_end_date FROM lots '
-        . 'JOIN categories ON lots.category_id = categories.id';
+// Собираем запрос для получения саиска лотов для главной.
+$ads_sql = 'SELECT lots.title, categories.name AS category, lots.start_price AS price,'
+    . ' lots.img, lots.finish_date AS auction_end_date FROM lots '
+    . 'JOIN categories ON lots.category_id = categories.id';
 
-    // Выаолняем запрос.
-    $ads_result = mysqli_query($db_connect, $ads_sql);
-    if (!$ads_result) {
-        $errors[] = 'Ошибка в sql запросе: ' . mysqli_error($db_connect);
-    }
-
-    // Конвертируем данные в массив.
-    $ads = mysqli_fetch_all($ads_result, MYSQLI_ASSOC);
+// Выаолняем запрос.
+$ads_result = mysqli_query($db_connect, $ads_sql);
+if (!$ads_result) {
+    die('Ошибка в sql запросе: ' . mysqli_error($db_connect));
 }
 
-if ($db_connect) {
-    // Собираем запрос для получения саиска категорий.
-    $categories_sql = 'SELECT * FROM categories';
+// Конвертируем данные в массив.
+$ads = mysqli_fetch_all($ads_result, MYSQLI_ASSOC);
 
-    // Выполняем запрос.
-    $categories_result = mysqli_query($db_connect, $categories_sql);
-    if (!$categories_sql) {
-        $errors[] = 'Ошибка в sql запросе: ' . mysqli_error($db_connect);
-    }
+// Собираем запрос для получения саиска категорий.
+$categories_sql = 'SELECT * FROM categories';
 
-    // Конвертируем данные в массив.
-    $categories = mysqli_fetch_all($categories_result, MYSQLI_ASSOC);
+// Выполняем запрос.
+$categories_result = mysqli_query($db_connect, $categories_sql);
+if (!$categories_sql) {
+    die('Ошибка в sql запросе: ' . mysqli_error($db_connect));
 }
 
-if ($errors === []) {
-    $content = include_template('main.php', [
-        'categories' => $categories,
-        'ads' => $ads
-    ]);
-} else {
-    $content = include_template('error.php', ['errors' => $errors]);
-}
+// Конвертируем данные в массив.
+$categories = mysqli_fetch_all($categories_result, MYSQLI_ASSOC);
+
+$content = include_template('main.php', [
+    'categories' => $categories,
+    'ads' => $ads
+]);
 
 $layout = include_template('layout.php', [
     'title' => 'Yeti Cave | Главная',
@@ -65,4 +49,5 @@ $layout = include_template('layout.php', [
     'categories' => $categories,
     'content' => $content
 ]);
+
 print($layout);
