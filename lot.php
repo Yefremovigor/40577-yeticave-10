1d4dcba5358477ef76c0586e9f9e51555877f8c1
Игорь <Yefremovigor@yandex.ru>
2019-09-01 09:25:53 +0300

    Добавлеяет сценарий и шаблон для отображения страниыц лота

---
+++ lot.php
@@ -0,0 +1,41 @@
+<?php
+// Проверямем авторизацию.
+$is_auth = rand(0, 1);
+$user_name = 'Игорь';
+
+// Подключаем базу данных.
+require_once('db.php');
+// Поподключаем функции.
+require_once('helpers.php');
+require_once('functions.php');
+// Задаем часовой пояс
+date_default_timezone_set('Europe/Moscow');
+
+// Проверяем GET параметр
+if (empty($_GET['lot_id'])) {
+    // Формируем 404 заголовок.
+    header("HTTP/1.x 404 Not Found");
+
+    // Окончаем выволнение сценария.
+    die();
+}
+
+// Собираем запрос для получения саиска категорий.
+$categories_sql = 'SELECT * FROM categories';
+
+// Выполняем запрос и конвертируем данные в двумерный массив.
+$categories = get_data_frob_db($categories_sql, $db_connect);
+
+$content = include_template('lot-template.php', [
+    'categories' => $categories
+]);
+
+$layout = include_template('layout.php', [
+    'title' => 'Yeti Cave | Название_товара',
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+    'categories' => $categories,
+    'content' => $content
+]);
+
+print($layout);
