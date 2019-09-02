93310caa9db9a2827ea77028f9697cad2ccf6ff9
Игорь <Yefremovigor@yandex.ru>
2019-09-02 09:57:40 +0300

    Добавляет шаблон вывода меню и подключает его в габлон лота

--- templates/lot-template.php
+++ templates/lot-template.php
@@ -1,25 +1,5 @@
-<nav class="nav">
-    <ul class="nav__list container">
-        <li class="nav__item">
-            <a href="all-lots.html">Доски и лыжи</a>
-        </li>
-        <li class="nav__item">
-            <a href="all-lots.html">Крепления</a>
-        </li>
-        <li class="nav__item">
-            <a href="all-lots.html">Ботинки</a>
-        </li>
-        <li class="nav__item">
-            <a href="all-lots.html">Одежда</a>
-        </li>
-        <li class="nav__item">
-            <a href="all-lots.html">Инструменты</a>
-        </li>
-        <li class="nav__item">
-            <a href="all-lots.html">Разное</a>
-        </li>
-    </ul>
-</nav>
+<?php require_once('menu.php') ?>
+
 <section class="lot-item container">
     <h2>DC Ply Mens 2016/2017 Snowboard</h2>
     <div class="lot-item__content">
