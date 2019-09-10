a6dd80eba777dbe1ef73aba993c2faa1ff285dad
Игорь <Yefremovigor@yandex.ru>
2019-09-10 15:54:29 +0300

    Добавляет подсветку полей в которых ошибка

--- templates/add-template.php
+++ templates/add-template.php
@@ -1,13 +1,13 @@
 <?php require_once('menu.php') ?>
-<form class="form form--add-lot container form--invalid" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
+<form class="form form--add-lot container <?=(isset($errors)) ? 'form--invalid' : '' ?>" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
     <h2>Добавление лота</h2>
     <div class="form__container-two">
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+        <div class="form__item <?=(isset($errors['lot-name'])) ? 'form__item--invalid' : '' ?>">
             <label for="lot-name">Наименование <sup>*</sup></label>
             <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" value="<?=getPostVal('lot-name') ?>">
             <span class="form__error">Введите наименование лота</span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?=(isset($errors['category'])) ? 'form__item--invalid' : '' ?>">
             <label for="category">Категория <sup>*</sup></label>
             <select id="category" name="category">
                 <?php foreach ($categories as $category): ?>
@@ -17,34 +17,33 @@
             <span class="form__error">Выберите категорию</span>
         </div>
     </div>
-    <div class="form__item form__item--wide">
+    <div class="form__item form__item--wide <?=(isset($errors['message'])) ? 'form__item--invalid' : '' ?>">
         <label for="message">Описание <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите описание лота">
-            <?=getPostVal('message') ?>
-        </textarea>
+        <textarea id="message" name="message" placeholder="Напишите описание лота"><?=getPostVal('message') ?></textarea>
         <span class="form__error">Напишите описание лота</span>
     </div>
-    <div class="form__item form__item--file">
+    <div class="form__item form__item--file <?=(isset($errors['lot-img'])) ? 'form__item--invalid' : '' ?>">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
             <input class="visually-hidden" type="file" name="lot-img" id="lot-img" value="">
             <label for="lot-img">
                 Добавить
             </label>
+            <span class="form__error">Добасьте фотографию лота</span>
         </div>
     </div>
     <div class="form__container-three">
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?=(isset($errors['lot-rate'])) ? 'form__item--invalid' : '' ?>">
             <label for="lot-rate">Начальная цена <sup>*</sup></label>
             <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?=getPostVal('lot-rate') ?>">
             <span class="form__error">Введите начальную цену</span>
         </div>
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?=(isset($errors['lot-step'])) ? 'form__item--invalid' : '' ?>">
             <label for="lot-step">Шаг ставки <sup>*</sup></label>
             <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?=getPostVal('lot-step') ?>">
             <span class="form__error">Введите шаг ставки</span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?=(isset($errors['lot-date'])) ? 'form__item--invalid' : '' ?>">
             <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
             <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?=getPostVal('lot-date') ?>">
             <span class="form__error">Введите дату завершения торгов</span>
