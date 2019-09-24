6e45b942614701a7cf59a4da38a34d16c7262f46
Игорь <Yefremovigor@yandex.ru>
2019-09-24 17:38:02 +0300

    Добавляет подсветку ошибок

--- templates/login-template.php
+++ templates/login-template.php
@@ -1,12 +1,12 @@
 <?php require_once('menu.php') ?>
-<form class="form container" action="login.php" method="post"> <!-- form--invalid -->
+<form class="form container <?=(isset($errors)) ? 'form--invalid' : '' ?>" action="login.php" method="post">
     <h2>Вход</h2>
-    <div class="form__item"> <!-- form__item--invalid -->
+    <div class="form__item <?=(isset($errors['email'])) ? 'form__item--invalid' : '' ?>">
         <label for="email">E-mail <sup>*</sup></label>
         <input id="email" type="text" name="email" placeholder="Введите e-mail">
         <span class="form__error">Введите e-mail</span>
     </div>
-    <div class="form__item form__item--last">
+    <div class="form__item form__item--last <?=(isset($errors['password'])) ? 'form__item--invalid' : '' ?>">
         <label for="password">Пароль <sup>*</sup></label>
         <input id="password" type="password" name="password" placeholder="Введите пароль">
         <span class="form__error">Введите пароль</span>
