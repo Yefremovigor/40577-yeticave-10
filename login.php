db72974d33a49023a883c82941baddd1fc6c7005
Игорь <Yefremovigor@yandex.ru>
2019-09-24 17:34:06 +0300

    Добавляет скрипт и шаблон раздела входв на сайт

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+// Проверямем авторизацию.
+$is_auth = rand(0, 1);
+$user_name = 'Игорь';
+
+// Подключаем базу данных.
+require_once('db.php');
+// Поподключаем функции.
+require_once('helpers.php');
+require_once('functions.php');
+// Задаем часовой пояс
+date_default_timezone_set('Europe/Moscow');
+
+// Собираем запрос для получения саиска категорий.
+$categories_sql = 'SELECT * FROM categories';
+
+// Выполняем запрос и конвертируем данные в двумерный массив.
+$categories = get_data_from_db($categories_sql, $db_connect);
+
+$content = include_template('login-template.php', [
+    'categories' => $categories
+]);
+
+$layout = include_template('layout.php', [
+    'title' => 'Yeti Cave | Название_товара',
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+    'categories' => $categories,
+    'content' => $content
+]);
+
+print($layout);
