1d4dd9eaac9f005341fae1464cfc047ca0df35f8
Игорь <Yefremovigor@yandex.ru>
2019-10-08 20:47:48 +0300

    Добавляет менеджер пакетов

--- init.php
+++ init.php
@@ -4,6 +4,9 @@ session_start();
 $is_auth = (isset($_SESSION['user'])) ? 1 : 0;
 $user_name = (isset($_SESSION['user'])) ? $_SESSION['user']['name'] : '';
 
+// Подключаем пакеты из composer
+require_once('vendor/autoload.php');
+
 // Подключаем базу данных.
 require_once('db.php');
 // Поподключаем функции.
